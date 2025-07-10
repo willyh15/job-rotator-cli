291f0e845caa523024e8e2f5ecacaad41c537d96
Will Harrison <willharrison2016@gmail.com>
2025-07-10 11:33:03 -0500

    Create svelte.config.js

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: undefined // we’ll specify Vercel or static adapter later
+  }
+};
+
+export default config;
