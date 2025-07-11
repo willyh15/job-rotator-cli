3a528718b44ba391c243a83bc73677fc2cf9841c
root <root@racknerd-08608fbembroideryfiles.duckdns.org>
2025-07-11 02:28:25 +0000

    Fix base path for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 export default {
   preprocess: preprocess(),
-
   kit: {
     adapter: adapter({
+      pages: 'build',
+      assets: 'build',
       fallback: 'index.html'
     }),
+    paths: {
+      base: dev ? '' : '/job-rotator-cli' // 👈 set to repo name
+    },
     prerender: {
-      entries: []
+      entries: ['*']
     }
   }
 };
