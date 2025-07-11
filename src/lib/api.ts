b579804376fbde4533963c5cbe8de80242f7e0ce
Will Harrison <willharrison2016@gmail.com>
2025-07-10 21:38:18 -0500

    Delete src directory

--- src/lib/api.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchJobs(): Promise<any[]> {
-  const res = await fetch('/job_results.json');
-  if (!res.ok) throw new Error('Failed to load jobs');
-  return res.json();
-}
--- src/lib/types.ts
+++
@@ -1,4 +0,0 @@
-export interface JobEntry {
-	title: string;
-	url: string;
-}
